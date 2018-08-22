7a764015d47b4b8edefcdce670ee94c9bf243f27
Chris Keller <cjameskeller@gmail.com>
2018-08-21 21:53:37 -0500

    trucking along

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,57 @@
 public class Main {
 
     public static void main(String[] args) {
+        int linesOfCode;
+        int yearsExp;
+        int bonus;
+        int jobChance;
+        String result;
 
-        boolean firstProject = true;
-        int linesOfCode = 9000;
-        int yearsExp = 1;
-        int bonus = 1000;
+        linesOfCode = 9000;
+        yearsExp = 1;
+        bonus = 1000;
 
-        int jobChance = calcJobChance(firstProject, linesOfCode, yearsExp, bonus);
-        System.out.println("Your chances of getting a job are " + jobChance);
+        jobChance = calcJobChance(linesOfCode, yearsExp, bonus);
+        result = resultString(jobChance);
+
+        System.out.println(result + jobChance);
     }
 
-    public static int calcJobChance(boolean firstProject, int linesOfCode, int yearsExp, int bonus) {
-        if (firstProject) {
-            int chances = linesOfCode + (yearsExp * bonus);
-            return chances;
+    private static boolean isFirstProject() {
+        return (Math.random() < 0.4);
+    }
+
+    private static boolean isCustomerFacing() {
+        return (Math.random() < 0.6);
+    }
+
+    private static int calcJobChance(int linesOfCode, int yearsExp, int bonus) {
+        if (isFirstProject()) {
+            return linesOfCode + (yearsExp * bonus);
         }
 
         return -1;
+    }
+
+    private static String resultString(int jobChance) {
+        String result;
+        int minimum;
+
+        result = "";
+        minimum = 1000;
+
+        if (isCustomerFacing()) {
+            if (jobChance < minimum) {
+                result += "We're sorry, ";
+            } else {
+                result += "Congratulations, ";
+            }
+
+            result += "your ";
+        }
+
+        result += "chances of getting a job are ";
+
+        return result;
     }
 }

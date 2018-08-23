79ea660903ca441cc74ed3760b66b2da4686b1c1
Chris Keller <cjameskeller@gmail.com>
2018-08-23 11:46:01 -0500

    add in more stuff

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,15 +8,58 @@ public class Main {
         int bonus;
         int jobChance;
         String result;
+        int count;
+        String yearsOfExperience;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many years of experience do you have?");
+        yearsOfExperience = scanner.nextLine();
 
         linesOfCode = 9000;
-        yearsExp = 1;
-        bonus = 1000;
+        bonus = 0;
+        count = 5;
 
-        jobChance = calcJobChance(linesOfCode, yearsExp, bonus);
+        yearsExp = Integer.parseInt(yearsOfExperience);
+
+        if (isGettingBonus(linesOfCode, yearsExp)) {
+            bonus = 1000;
+        }
+
+        if ((bonus > 0) && (yearsExp > 1)) {
+            jobChance = calcJobChance(linesOfCode, yearsExp, bonus);
+        } else {
+            jobChance = calcJobChance(linesOfCode);
+        }
+
         result = resultString(jobChance);
 
+        System.out.println("Counting down to results:");
+
+        while (count != 0) {
+            System.out.println(count);
+            count--;
+        }
+
         System.out.println(result + jobChance);
+
+        scanner.close();
+    }
+
+    private static boolean isGettingBonus(int linesOfCode, int yearsExp) {
+        switch (yearsExp) {
+            case 0:
+                return (linesOfCode > 1000);
+            case 1:
+                return (linesOfCode > 5000);
+            case 2:
+            case 3:
+            case 4:
+                return (linesOfCode > (yearsExp * 7000));
+            case 5:
+                return (linesOfCode > 5000);
+            default:
+                return false;
+        }
     }
 
     private static boolean isFirstProject() {
@@ -25,12 +70,20 @@ public class Main {
         return (Math.random() < 0.6);
     }
 
-    private static int calcJobChance(int linesOfCode, int yearsExp, int bonus) {
+    private static int calcJobChance(int linesOfCode) {
         if (isFirstProject()) {
+            return 0;
+        }
+
+        return linesOfCode;
+    }
+
+    private static int calcJobChance(int linesOfCode, int yearsExp, int bonus) {
+        if (!isFirstProject()) {
             return linesOfCode + (yearsExp * bonus);
         }
 
-        return -1;
+        return 0;
     }
 
     private static String resultString(int jobChance) {
